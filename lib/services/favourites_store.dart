a9999b315a7997f890e0fb17d349fd77c7e6e886
Sruthi Rajesh <113259531+Sruthi-rajesh@users.noreply.github.com>
2026-02-26 13:14:20 +1100

    Update favourites_store.dart

--- lib/services/favourites_store.dart
+++ lib/services/favourites_store.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/foundation.dart';
 
 class FavouritesStore {
-  /// Stores Place IDs
   static final ValueNotifier<Set<String>> favIds =
       ValueNotifier<Set<String>>(<String>{});
 
-  /// Backwards compatible (older code might call favouriteIds)
   static Set<String> get favouriteIds => favIds.value;
 
-  /// Backwards compatible (some code might call listenable)
   static ValueListenable<Set<String>> get listenable => favIds;
 
   static bool isFavourite(String id) => favIds.value.contains(id);
